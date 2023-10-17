52942ef51c3386d148fefb71284657f4e8d6a715
lss <sslee@spolive.com>
2023-10-17 14:51:16 +0900

    셋팅

---
+++ home/views.py
@@ -0,0 +1,10 @@
+from rest_framework import viewsets, permissions, generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+
+# Create your views here.
+@api_view(['GET'])
+def home(request):
+    return Response('home api');
+    
